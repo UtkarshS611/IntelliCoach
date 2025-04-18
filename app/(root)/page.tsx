cb798ce11504b856ec5bed0ade2e476ba5acc5fd
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2025-04-18 20:05:05 +0530

    auth routes and root routes updated

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-6xl">heyyyyyyyyyyyyy Homeeeeeeeeeeee</h1>
+    </section>
+  );
+}
